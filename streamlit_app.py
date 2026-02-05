0ce71d2a8545f4ee87a0679c20b70f4208847ef5
LTCcounts <stashalseiu2015@gmail.com>
2026-02-05 21:03:36 +0000

    uh

--- streamlit_app.py
+++ streamlit_app.py
@@ -52,9 +52,14 @@ months = st.slider("Month", 1, 12, (1, 12))
 # Filter the dataframe based on the widget input and reshape it.
 df_filtered = df[(df["Chapter"].isin(chapters)) & (df["Year"].between(years[0], years[1])) & (df["Month"].between(months[0], months[1]))]
 df_reshaped = df_filtered.pivot_table(
-    index="Year", columns="Chapter", values=["General Fund","Savings/Strike"], aggfunc="sum", fill_value=0
+    index="Year", columns="Chapter", values=["General Fund"], aggfunc="sum", fill_value=0
 )
 df_reshaped = df_reshaped.sort_values(by="Year", ascending=False)
+
+df_reshaped2 = df_filtered.pivot_table(
+    index="Year", columns="Chapter", values=["Savings/Strike"], aggfunc="sum", fill_value=0
+)
+df_reshaped2 = df_reshaped2.sort_values(by="Year", ascending=False)
 
 #Columns: Year,Month,Date,Chapter,General Fund,Savings/Strike
 # Display the data as a table using `st.dataframe`.
@@ -65,10 +70,38 @@ st.dataframe(
 )
 #Columns: Year,Month,Date,Chapter,General Fund,Savings/Strike
 # Display the data as an Altair chart using `st.altair_chart`.
-
-st.bar_chart(
-    df,
-    x="Year",
-    y=["General Fund", "Savings/Strike"],
-    color=["#00ff00", "#339933"]
+df_chart = pd.melt(
+    df_reshaped.reset_index(), id_vars="Year", var_name="Chapter", value_name="General Fund"
 )
+chart = (
+    alt.Chart(df_chart)
+    .mark_line()
+    .encode(
+        x=alt.X("Year:N", title="Year"),
+        y=alt.Y("General Fund:Q", title="General Fund ($)"),
+        color="Chapter:N",
+    )
+    .properties(height=320)
+)
+col1, col2 = st.columns(2)
+
+df_chart2 = pd.melt(
+    df_reshaped2.reset_index(), id_vars="Year", var_name="Chapter", value_name="Savings/Strike"
+)
+chart2 = (
+    alt.Chart(df_chart2)
+    .mark_line()
+    .encode(
+        x=alt.X("Year:N", title="Year"),
+        y=alt.Y("Savings/Strike :Q", title="Savings/Strike ($)"),
+        color="Chapter:N",
+    )
+    .properties(height=320)
+)
+col1, col2 = st.columns(2)
+
+with col1:
+    st.altair_chart(chart, use_container_width=True)
+
+with col2:
+    st.altair_chart(chart2, use_container_width=True)
