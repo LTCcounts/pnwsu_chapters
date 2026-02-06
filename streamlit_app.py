43ef75d588aec15e178a5235f571cc8b1b20ea05
LTCcounts <stashalseiu2015@gmail.com>
2026-02-06 04:15:01 +0000

    filter

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ st.image("data/Picture1.png")
 df = pd.read_csv("data/chapter_bal.csv")
 df = df[["Chapter","Year","Date","General Fund","Savings/Strike"]]
 
-df_filtered = df[(df["Chapter"]=="PROTEC17")]
+df_filtered = df[(df["Chapter"]=="SEIU 2015")]
 #chart_df["Year"] = chart_df.index
 st.line_chart(df_filtered, x="Date", y=["General Fund"])
 st.image("data/border_img.png")
