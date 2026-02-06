c33bd951a71d3ed7cc5734877d676404bf4508f9
LTCcounts <stashalseiu2015@gmail.com>
2026-02-06 03:52:44 +0000

    no agg

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ import streamlit as st
 st.image("data/Picture1.png")
 df = pd.read_csv("data/chapter_bal.csv")
 df = df[["Chapter","Year","General Fund","Savings/Strike"]]
-chart_df = df.groupby(["Year"]).mean()
+chart_df = df.groupby(["Year"])
 chart_df["Year"] = chart_df.index
 st.line_chart(chart_df, x="Year", y=["General Fund"])
 st.line_chart(chart_df, x="Year", y=["Savings/Strike"])
