35766307b983ea7b19ea4551957a39f5967cee9d
LTCcounts <stashalseiu2015@gmail.com>
2026-02-06 02:11:35 +0000

    password

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,13 +3,13 @@ import pandas as pd
 import streamlit as st
 
 # Common widgets that persist across all pages
-user_name = st.sidebar.text_input("Name", key="password")
-user_role = st.sidebar.selectbox("Chapter", ["BSSU", "PROTEC17"], key="passowrd")
+user_name = st.sidebar.text_input("Password", key="password")
+user_role = st.sidebar.selectbox("Chapter", ["BSSU", "PROTEC17"], key="chapter")
 
 # Navigation
 page = st.navigation([
     st.Page("page1.py", title="Dashboard"),
-    st.Page("page2.py", title="Settings"),
+    #st.Page("page2.py", title="Settings"),
 ])
 page.run()
 
