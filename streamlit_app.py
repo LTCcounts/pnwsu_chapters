94378c016bb579b56430854b6ce85f28127a96d1
LTCcounts <stashalseiu2015@gmail.com>
2026-02-06 00:00:48 +0000

    ...

--- streamlit_app.py
+++ streamlit_app.py
@@ -53,12 +53,12 @@ chapters = st.selectbox("Select your chapter",
 
 # Show a slider widget with the years using `st.slider`.
 years = st.slider("Year", 2023, 2026, (2024, 2026))
-months = st.slider("Month", 1, 12)
+months = st.slider("Month", 1, 12, (1,12))
 st.write(f"Account balances for: {chapters}") 
 
 
 # Filter the dataframe based on the widget input and reshape it.
-df_filtered = df[(df["Chapter"]==chapters) & (df["Year"].between(years[0], years[1])) & (df["Month"].between([0], months[1]))]
+df_filtered = df[(df["Chapter"]==chapters) & (df["Year"].between(years[0], years[1])) & (df["Month"].between(months[0], months[1]))]
 
 #DF Reshape 0
 df_reshaped0 = df_filtered.pivot_table(
