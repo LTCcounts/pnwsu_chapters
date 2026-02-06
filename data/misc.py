0b2ccbfe7ace09e68a9961f5ac7c56ab6c831511
LTCcounts <stashalseiu2015@gmail.com>
2026-02-06 03:50:12 +0000

    lets see

--- data/misc.py
+++ data/misc.py
@@ -1,15 +1,7 @@
-import altair as alt
-import pandas as pd
-import streamlit as st
-
-st.image("data/Picture1.png")
 df = pd.read_csv("data/chapter_bal.csv")
 df = df[["Chapter","Year","General Fund","Savings/Strike"]]
 chart_df = df.groupby(["Year"]).mean()
 chart_df["Year"] = chart_df.index
 st.line_chart(chart_df, x="Year", y=["General Fund"])
 st.line_chart(chart_df, x="Year", y=["Savings/Strike"])
-
-
-
-
+#Year,Month,Date,Chapter,General Fund,Savings/Strike
