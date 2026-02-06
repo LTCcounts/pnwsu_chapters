0ce92e2e9a57866c889025f0a761b5438e13a0c9
LTCcounts <stashalseiu2015@gmail.com>
2026-02-06 04:04:33 +0000

    chart

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,10 +6,10 @@ st.image("data/Picture1.png")
 df = pd.read_csv("data/chapter_bal.csv")
 df = df[["Chapter","Year","General Fund","Savings/Strike"]]
 chart_df = df.groupby(["Chapter","Year","Month","Date"])
-df_filtered = df[(df["Chapter"]=="BSSU")]
+df_filtered = chart_df[(df["Chapter"]=="BSSU")]
 #chart_df["Year"] = chart_df.index
-st.line_chart(chart_df, x="Year", y=["General Fund"])
-st.line_chart(chart_df, x="Year", y=["Savings/Strike"])
+st.line_chart(df_filtered, x="Year", y=["General Fund"])
+st.line_chart(df_filtered, x="Year", y=["Savings/Strike"])
 
 
 
